8671a9e098c5df56125c3bc0438c6e19df5a93af
dilmun12535 <121222999+dilmun12535@users.noreply.github.com>
2026-06-18 23:27:23 +0900

    Update meal-check.js

--- js/meal-check.js
+++ js/meal-check.js
@@ -1,4 +1,4 @@
-const CARE_PLAN_API_URL = "https://script.google.com/macros/s/AKfycbfaEN0MkkWd_NnDif5LXlCVbIxqgllvGLoJturv0FlXtgX1FG0QTVQNArI5DyR5RTZaA/exec";
+const CARE_PLAN_API_URL = "https://script.google.com/macros/s/AKfycbxFaEN0MkkWd_NnDif5LXlCVbIxqgllvGLoJturv0FlXtgX1FG0QTVQNArI5DyR5RTZaA/exec";
 
 let carePlanLibraryCache = [];
 let counselLibraryCache = [];
