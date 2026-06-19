f47dcf3b0d8fa0af26a5661313c261c45f41d533
dilmun12535 <121222999+dilmun12535@users.noreply.github.com>
2026-06-19 13:51:04 +0900

    Update nursing-vital-check.js

--- js/nursing-vital-check.js
+++ js/nursing-vital-check.js
@@ -1,4 +1,4 @@
-const CARE_PLAN_API_URL = "https://script.google.com/macros/s/AKfycbx4ZrUg65tAdpMWLdPH30LBxLehX4331VIy81-OqL45b8oA0cdi4gHtByhO9K-rViF8Mw/exec";
+const CARE_PLAN_API_URL = "https://script.google.com/macros/s/AKfycbySI7LOKA-dS3reMpJsGhscD9e2TK_gtTs6BwWYBiN88GGyNFnDvjXK91ldsUbPhFox/exec";
 
 let carePlanLibraryCache = [];
 let counselLibraryCache = [];
