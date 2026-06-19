47c34034f1b543825c6e87c3d36894791266fd64
dilmun12535 <121222999+dilmun12535@users.noreply.github.com>
2026-06-19 16:18:21 +0900

    Update care-plan-library.js

--- js/care-plan-library.js
+++ js/care-plan-library.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxFfVUr6hOEpYJbPxJxCW_TOMR144lqoz7Gir9kDZMTFOCy-ygrfrQ0YLzPxfx5aEzZbQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwPzdlpnsyEX4nr1nliw_8JQX6J2Fgcv0B6t0dGKxuUtCCVrTjLnnHAreWBErgrUs2a_A/exec";
 
 // HTML 내부 변수와 부딪히지 않도록 스크립트 전용 고유 이름으로 안전하게 요소를 매칭합니다.
 const elPlanFileSelector = document.getElementById("planFile");
